101645cdb058b5eda2c5e7585852abfe0d73558e
brunoschmitt <bruno_alexandre.s@hotmail.com>
2020-12-16 01:20:04 -0300

    Calculadora

--- ConsoleApp3/Calculadora.cs
+++ ConsoleApp3/Calculadora.cs
@@ -1,4 +1,6 @@
-﻿namespace Projeto
+﻿using System;
+
+namespace Projeto
 {
     public class Calculadora
     {
@@ -7,6 +9,7 @@
         {
             return n1 + n2;
         }
+
         public double Soma(params double[] numeros)
         {
             double resultado = 0.0;
@@ -16,5 +19,33 @@
             }
             return resultado;
         }
+
+        public double Subtracao(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Multiplicacao(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public double Divisao(double n1, double n2)
+        {
+            double resultado = 0D;
+
+            if (n2 != 0)
+            {
+                resultado = n1 / n2;
+            }
+            else
+            {
+                Console.Write($"Não é possivel efetuar divisão por 0");
+                return n1;
+            }
+
+            return resultado;
+        }
+
     }
 }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,23 +7,72 @@ namespace Projeto
     { 
         public static void Main(String[] args)
         {
-            Calculadora calc;
-            calc = new Calculadora();
 
-            double[] numeros = { 1.2, 1.3, 1.4, 2.4, 4.4 };
-
-            double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double resultado = calc.Soma(n1, n2);
-            Console.WriteLine("resultado 1 = " + resultado);
-
-
-            double resultado2 = calc.Soma(numeros);
-            Console.WriteLine("resultado 2 =" + resultado2);
-
-
+            IniciaCalculadora();
             Console.Read();
+        }
+
+        public static void EntradaDeDados(out double n1, ref string operacao, string operacaoPlus)
+        {
+            Console.WriteLine("Insira um numero");
+            n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(Menu(operacaoPlus));
+            operacao = Console.ReadLine();
+
+        }
+
+        public static string Menu(string operacao)
+        {
+            return "Escolha uma operação : \n" +
+               "+ (soma)\n" +
+               "- (subtração) \n" +
+               "* (multiplicação) \n" +
+               "/ (divisão) \n" +
+               operacao;
+        }
+
+        public static void IniciaCalculadora()
+        {
+
+            Calculadora calculadora = new Calculadora();
+
+            double n1;
+
+            double resultado = 0D;
+            string operacao = "";
+
+
+
+            EntradaDeDados(out n1, ref operacao, "");
+            resultado = n1;
+            do
+            {
+                if (operacao.Trim().Equals("+"))
+                {
+                    EntradaDeDados(out n1, ref operacao, "= (Resultado)");
+                    resultado = calculadora.Soma(resultado, n1);
+                   
+                } else if (operacao.Trim().Equals("-"))
+                {
+                    EntradaDeDados(out n1, ref operacao, "= (Resultado)");
+                    resultado = calculadora.Subtracao(resultado, n1);
+                } else if (operacao.Trim().Equals("*"))
+                {
+                    EntradaDeDados(out n1, ref operacao, "= (Resultado)");
+                    resultado = calculadora.Multiplicacao(resultado, n1);
+                } else if (operacao.Trim().Equals("/"))
+                {
+                    EntradaDeDados(out n1, ref operacao, "= (Resultado)");
+                    resultado = calculadora.Divisao(resultado, n1);
+                } else
+                {
+                    Console.WriteLine("Operação Indisponivel");
+                }
+
+            } while (!operacao.Equals("="));
+
+            Console.WriteLine(resultado);
         }
     }
 
